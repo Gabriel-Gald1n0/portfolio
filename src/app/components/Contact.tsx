b71a97b956cc152ee3e3f44f00264f4e9c24e7c2
Gabriel Galdino <galdinobrossx1@gmail.com>
2026-01-19 01:28:21 -0300

    feat: Update EmailJS integration to use Vite environment variables

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -18,10 +18,10 @@ export function Contact() {
 
     emailjs
       .sendForm(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         formRef.current!,
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
